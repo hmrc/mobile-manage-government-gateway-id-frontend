5d66e5e7dec568f4ad0880d1a3583b02ba307cf3
JavedBeg-ac <181977665+JavedBeg-ac@users.noreply.github.com>
2025-10-22 13:09:35 +0100

    HMA-10811 dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val playBootstrapVersion = "10.1.0"
+  private val playBootstrapVersion = "10.3.0"
   private val playFrontendVersion  = "12.11.0"
   private val scalaMockVersion     = "7.4.0"
 
